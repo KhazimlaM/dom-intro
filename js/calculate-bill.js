2385611b770513fd86115d825defea1970782193
Khazimla <20113497@rcconnect.co.za>
2022-05-10 15:26:03 +0200

    third changes

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,12 +1,13 @@
 //get a reference to the calculate button
 const calculateBtn = document.querySelector(".calculateBtn")
 
-
 //get a reference to the billTotal element
 const billTotalElement = document.querySelector(".billTotal")
 
 //get a reference to the billString
 const billStringElement = document.querySelector(".billString")
+
+
 
 //create the function that will be called when the calculate button is pressed
 function totalPhoneBill(callsMade){
@@ -15,18 +16,20 @@ function totalPhoneBill(callsMade){
      
      for(var i =0;i<parameter.length;i++){
      var checkCallsMade = parameter[i].trim();
+     var upperCase = checkCallsMade.toLowerCase()
        
        
-       if(checkCallsMade === "call"){
+       if(upperCase=== "call"){
        totalBill += 2.75;
        }
-       else if(checkCallsMade === "sms"){
+       else if(upperCase === "sms"){
        totalBill += 0.65;
        }
      }
      return totalBill.toFixed(2);
-   }
 
+   }
+  
 // add event listener
 function calculateBtnClicked(){
     // get the string entered in the textArea
@@ -37,17 +40,20 @@ function calculateBtnClicked(){
     billTotalElement.innerHTML = roundedBillTotal;
 
     // complete if statement for red and orange color change
+    if(roundedBillTotal < 20){
+      billTotalElement.classList.remove("danger");
+      billTotalElement.classList.remove("warning");
+    }
 
     if(roundedBillTotal >= 30){
-        billTotalElement.classList.add("danger");
-     }
-   else if(roundedBillTotal >= 20){
-        billTotalElement.classList.add("warning");
-     }
-}
-     
-calculateBtn.addEventListener('click', calculateBtnClicked);
+      billTotalElement.classList.add("danger");
+    }
+    else if(roundedBillTotal >= 20){
+      billTotalElement.classList.add("warning");
+    } 
+  }
 
+  calculateBtn.addEventListener('click', calculateBtnClicked);
 
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
